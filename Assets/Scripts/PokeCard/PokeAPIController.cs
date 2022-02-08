f3ef7004330d9c2536325b6327487c05b48fd995
SamBlesswin <samblesswinedu@gmail.com>
2022-02-08 15:47:34 +0530

    Pokemon Cards Done

--- Assets/Scripts/PokeCard/PokeAPIController.cs
+++ Assets/Scripts/PokeCard/PokeAPIController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using UnityEngine.Networking;
+using SimpleJSON;
 
 public class PokeCard
 {
@@ -19,8 +20,10 @@ public class PokeAPIController : MonoBehaviour
     [SerializeField] private TMP_Text PokeID;
     [SerializeField] private TMP_Text PokeName;
     [SerializeField] private TMP_Text[] PokeTypes;
-
-    public Button RandomCard;
+    [SerializeField] private TMP_InputField SearchID;
+    [SerializeField] private Button Searchbtn;
+    [SerializeField] private Button RandomCard;
+    [SerializeField] private TMP_Text Error;
 
     private readonly string basePokeURL = "https://pokeapi.co/api/v2/";
 
@@ -30,26 +33,52 @@ public class PokeAPIController : MonoBehaviour
         {
             GetRandomPokemon();
         });
+
+        Searchbtn.onClick.AddListener(() =>
+        {
+            SearchPokemon();
+        });
     }
 
-    void GetRandomPokemon()
+    private void Reset()
     {
-        int randompokeID = Random.Range(1, 808);
+        Error.gameObject.SetActive(false);
         PokeImage.texture = Texture2D.blackTexture;
-        PokeID.text = "#" + randompokeID;
+        PokeID.text = "#";
         PokeName.text = "Loading...";
 
-        foreach(TMP_Text type in PokeTypes)
+        foreach (TMP_Text type in PokeTypes)
         {
             type.text = "";
         }
+    }
 
+    private void GetRandomPokemon()
+    {
+        Reset();
+        int randompokeID = Random.Range(1, 808);
+        PokeID.text = "#" + randompokeID;
         StartCoroutine(GetPokemonAtIndex(randompokeID));
     }
 
-    IEnumerator GetPokemonAtIndex(int randompokeID)
+    private void SearchPokemon()
     {
-        string pokemonURL = basePokeURL + "pokemon/" + randompokeID.ToString();
+        Reset();
+        int pokeID = int.Parse(SearchID.text);
+        if (SearchID.text!="" && (pokeID > 0 && pokeID < 808))
+        {
+            PokeID.text = "#" + pokeID;
+            StartCoroutine(GetPokemonAtIndex(pokeID));
+        }
+        else
+        {
+            Error.gameObject.SetActive(true);
+        }
+    }
+
+    IEnumerator GetPokemonAtIndex(int pokeID)
+    {
+        string pokemonURL = basePokeURL + "pokemon/" + pokeID.ToString();
 
         UnityWebRequest pokeInfoRequest = UnityWebRequest.Get(pokemonURL);
         yield return pokeInfoRequest.SendWebRequest();
@@ -61,12 +90,39 @@ public class PokeAPIController : MonoBehaviour
             yield break;
         }
 
-        PokeCard card = new PokeCard();
-        card = JsonUtility.FromJson<PokeCard>(pokeInfoRequest.downloadHandler.text);
-        PokeName.text = card.pokemonName;
-        for(int i=0;i<PokeTypes.Length;i++)
+        //Get pokemon name
+        JSONNode pokeInfo = JSON.Parse(pokeInfoRequest.downloadHandler.text);
+        string pokeName = pokeInfo["name"];
+
+        //Get pokemon sprite URL
+        string pokeSpriteURL = pokeInfo["sprites"]["front_default"];
+
+        //Get pokemon types in reverse
+        JSONNode pokeTypes = pokeInfo["types"];
+        string[] pokeTypeNames = new string[pokeTypes.Count];
+
+        for(int i=0,j=pokeTypes.Count-1;i<pokeTypes.Count;i++,j--)
         {
-            PokeTypes[i].text = card.pokemonTypes[i];
+            pokeTypeNames[i] = pokeTypes[j]["type"]["name"];
+        }
+
+        //Get pokemon image
+        UnityWebRequest pokeSpriteRequest = UnityWebRequestTexture.GetTexture(pokeSpriteURL);
+        yield return pokeSpriteRequest.SendWebRequest();
+
+        if (pokeSpriteRequest.result == UnityWebRequest.Result.ConnectionError
+            || pokeSpriteRequest.result == UnityWebRequest.Result.ProtocolError)
+        {
+            Debug.LogError(pokeSpriteRequest.error);
+            yield break;
+        }
+
+        //Set UI
+        PokeImage.texture = DownloadHandlerTexture.GetContent(pokeSpriteRequest);
+        PokeName.text = pokeName;
+        for(int i=0;i<pokeTypes.Count;i++)
+        {
+            PokeTypes[i].text = pokeTypeNames[i];
         }
     }
 }
